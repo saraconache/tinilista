0fa6acffd881aadde918ad1ff5a279d26eb0179e
Sarah Gomez <sarahvgomez@gmail.com>
2019-02-04 22:49:52 -0700

    Fixing mistakes

--- js/tinilista.js
+++ js/tinilista.js
@@ -1,11 +1,22 @@
 var input = document.querySelector("input[type= 'text']");
 var lista = document.querySelector("ul");
 
-input.addEventListener("keypress", function(pressedKey){
-// create a new item
-var item = document.createElement("li");
+input.addEventListener("keypress",function(pressedKey){
+
+    if(pressedKey.which === 13){
+// create a new item with icon wrapped in span
+var iteme = document.createElement("li");
 var addSpan = document.createElement("span");
 var icon = document.createElement("i");
+icon.innerHTML = "clear";
 
+var newItem = this.value;
+this.value = " ";
 
-})
+icon.classList.add('material-icons'); // add material Icons to span
+addSpan.append(icon);
+
+lista.appendChild(iteme).append(addSpan, newItem);
+
+    }
+});
