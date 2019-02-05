c47e6a090ce2208aee074e90b5857352f342918c
Sarah Gomez <sarahvgomez@gmail.com>
2019-02-04 22:38:47 -0700

    Attaching JS file, declaring variables to make this work.

---
+++ js/tinilista.js
@@ -0,0 +1,11 @@
+var input = document.querySelector("input[type= 'text']");
+var lista = document.querySelector("ul");
+
+input.addEventListener("keypress", function(pressedKey){
+// create a new item
+var item = document.createElement("li");
+var addSpan = document.createElement("span");
+var icon = document.createElement("i");
+
+
+})
