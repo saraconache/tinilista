6e44ed341d8a3e5d5689a1119670a1c233cd64ce
Sarah Gomez <sarahvgomez@gmail.com>
2019-02-05 08:22:28 -0700

    Default

--- js/tinilista.js
+++ js/tinilista.js
@@ -1,22 +1,31 @@
 var input = document.querySelector("input[type= 'text']");
 var lista = document.querySelector("ul");
+var spans = document.querySelectorAll('span');
+
 
 input.addEventListener("keypress",function(pressedKey){
-
     if(pressedKey.which === 13){
-// create a new item with icon wrapped in span
-var iteme = document.createElement("li");
-var addSpan = document.createElement("span");
-var icon = document.createElement("i");
-icon.innerHTML = "clear";
+        // create a new item with icon wrapped in span
+        var iteme = document.createElement("li");
+        var addSpan = document.createElement("span");
+        var icon = document.createElement("i");
+        icon.innerHTML = "clear";
 
-var newItem = this.value;
-this.value = " ";
+        var newItem = this.value;
+        this.value = " ";
 
-icon.classList.add('material-icons'); // add material Icons to span
-addSpan.append(icon);
+        icon.classList.add('material-icons'); // add material Icons to span
+        addSpan.append(icon);
 
-lista.appendChild(iteme).append(addSpan, newItem);
-
+        lista.appendChild(iteme).append(newItem, addSpan);
     }
 });
+
+
+lista.addEventListener('click', function(ev){
+    if(ev.target.tagName === "LI"){
+        ev.target.classList.toggle('checked');
+    }
+},false
+);
+
